e5f35e9ef57075664a4a68da17245181a273bec5
Lee Warrick <Lee.Warrick@realtruck.com>
2019-05-01 10:46:09 -0400

    remove ignored files

--- src/.temp/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  "siteUrl": "https://techjr.dev",
-  "siteName": "Gridsome",
-  "titleTemplate": "%s - Gridsome",
-  "siteDescription": "",
-  "version": "0.5.8"
-}
--- src/.temp/now.js
+++
@@ -1,1 +0,0 @@
-export default 1556657637884
